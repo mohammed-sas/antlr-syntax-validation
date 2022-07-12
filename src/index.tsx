4b1b11cb3aa7086f04bcfd4e5e50d498269c00c8
Mohammed <mohammed@Mohammeds-MacBook-Pro.local>
2022-07-12 10:40:45 +0530

    fix:cant find modules

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,9 @@
 import * as React from 'react';
 import {createRoot} from 'react-dom/client';
 import {setupLanguage} from "./script-lang/setup";
-// @ts-ignore
-import { Editor } from './components/Editor/Editor.tsx';
+import { Editor } from './components/Editor/Editor';
 import { languageID } from './script-lang/config';
-import { parseAndGetSyntaxErrors, parseAndGetASTRoot } from './language-service/Parser';
+
 
 setupLanguage();
 const App = () => <Editor language={languageID}></Editor>;
