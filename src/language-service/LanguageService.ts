d5d4fe952e3b35d7ca7fdb900c112f4563116852
Mohammed <mohammed@Mohammeds-MacBook-Pro.local>
2022-07-12 10:46:18 +0530

    fix:case sensitivity

--- src/language-service/LanguageService.ts
+++ src/language-service/LanguageService.ts
@@ -1,5 +1,5 @@
 
-import { parseAndGetASTRoot, parseAndGetSyntaxErrors } from "./Parser";
+import {  parseAndGetSyntaxErrors } from "./parser";
 import { IScriptError } from "./ScriptErrorListener";
 
 export default class ScriptService {
